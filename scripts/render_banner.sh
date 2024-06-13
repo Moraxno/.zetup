1af3e2fb49ac148b193a72d9040bd2ad2caa6b57
Robert Bock <robert.bock@imms.de>
2024-06-13 09:30:13 +0200

    minor changes

--- scripts/render_banner.sh
+++ scripts/render_banner.sh
@@ -7,9 +7,9 @@ echo '                      /_/          '
 echo ''
 
 tool_messages=()
-tool_messages+=(zimfw "Running Zsh IMproved." "Improvements missing") 
-tool_messages+=(stow "These .dotfiles are hearded." "The cattle is loose.") 
-tool_messages+=(junest "I might be jailed, but I have sudo." "Do not pass go, do not collect \$200.")
+tool_messages+=("zimfw" "Running Zsh IMproved." "Improvements missing") 
+tool_messages+=("stow" "These .dotfiles are hearded." "The cattle is loose.") 
+tool_messages+=("junest" "I might be jailed, but I have sudo." "Do not pass go, do not collect \$200.")
 
 longest_tool_name=0
 
@@ -20,7 +20,6 @@ for ((i = 1; i <= ${#tool_messages}; i+=3)); do
     fi
 done
 
-
 for ((i = 1; i <= ${#tool_messages}; i+=3)); do
     tool=${tool_messages[i]}
     pos=${tool_messages[i+1]}
@@ -29,7 +28,7 @@ for ((i = 1; i <= ${#tool_messages}; i+=3)); do
     tool_name_len=${#tool}
     pad_len=$((${longest_tool_name}-${tool_name_len}))
 
-    announce_tool $tool $pos $neg $pad_len
+    announce_tool $tool "$pos" "$neg" $pad_len
 done
 
 echo ""
